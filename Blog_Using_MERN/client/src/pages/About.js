ffa53dc6a5a63f92026b7d38deee9761ce4485ae
Sasindu Gunawardana <99400321+IT21279966@users.noreply.github.com>
2023-08-29 18:48:39 +0530

    Add files via upload

---
+++ Blog_Using_MERN/client/src/pages/About.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='mb-20'>
+      <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
+        About
+      </h1>
+
+      <p className='mx-auto leading-relaxed text-base mb-4'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum minima et suscipit officia omnis! Perferendis ut ipsam esse consectetur velit, dicta quas fugiat corporis tempora autem. Similique porro tempore, nisi voluptas blanditiis ex omnis vel officiis, in harum rerum, id pariatur sunt aspernatur qui quidem quasi alias aliquam cum soluta!
+      </p>
+      <p className='mx-auto leading-relaxed text-base mb-4'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum minima et suscipit officia omnis! Perferendis ut ipsam esse consectetur velit, dicta quas fugiat corporis tempora autem. Similique porro tempore, nisi voluptas blanditiis ex omnis vel officiis, in harum rerum, id pariatur sunt aspernatur qui quidem quasi alias aliquam cum soluta!
+      </p>
+      <p className='mx-auto leading-relaxed text-base mb-4'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, quisquam. Numquam unde sequi dolorum eius fuga quas pariatur, nam et repellat officia ipsa facilis eos. Corporis sint at repellendus maiores consequatur adipisci nam magnam, necessitatibus nulla commodi iure mollitia odio, modi soluta pariatur tempore doloribus suscipit? Eum saepe nihil aliquid? Expedita quaerat neque cumque quia suscipit inventore odio consectetur reiciendis accusantium at delectus, consequuntur amet assumenda similique perferendis quis explicabo maxime quibusdam enim, voluptate id eligendi deserunt ipsa magni! Repellendus odio consequatur corporis rerum saepe ullam voluptate delectus repudiandae, ipsam maiores nobis illum libero, sed magni aliquam voluptates consequuntur fuga.
+      </p>
+    </div>
+  )
+}
+
+export default About
+
